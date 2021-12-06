36ca5d12219d2d014c8628694880ac8ed632cecc
Nick <nbarrameda89@gmail.com>
2021-12-06 13:36:49 -0500

    separated spheres into its own class.

--- src/index.js
+++ src/index.js
@@ -1,66 +1,110 @@
-const Spheres = require("./spheres");
+import Sphere from './spheres.js';
 
 document.addEventListener("DOMContentLoaded", function () {
     const canvasMain = document.querySelector(".canvasMain");
-    const width = canvasMain.width = 900;
-    const height = canvasMain.height = 450;
-    const circle = canvasMain.getContext("2d");
-
-    function Circ(x, y, dx, dy, radius) {
-        this.x = x;
-        this.y = y;
-        this.dx = dx;
-        this.dy = dy;
-        this.radius = radius;
-        this.color = "green";
-
-        this.draw = function() {
-            circle.beginPath();
-            circle.arc(this.x, this.y, this.radius, 0, 2*Math.PI);
-            circle.fillStyle = this.color;
-            circle.fill();    
-        }
-
-        this.update = function() {
-            if (this.x + this.radius > width || this.x - this.radius < 0) this.dx = -this.dx;
-            if (this.y + this.radius > height || this.y - this.radius < 0) this.dy = -this.dy;
+    canvasMain.width = 900;
+    canvasMain.height = 550;
+    const width = canvasMain.width;
+    const height = canvasMain.height;
+    const sphere = canvasMain.getContext("2d");
     
-            this.x += this.dx;
-            this.y += this.dy;
+    // const circle = canvasMain.getContext("2d");
 
-            this.draw();
-        }
-    }
+    // function Circ(x, y, dx, dy, radius, style) {
+    //     this.x = x;
+    //     this.y = y;
+    //     this.dx = dx;
+    //     this.dy = dy;
+    //     this.radius = radius;
+    //     this.color = "gray";
+    //     this.style = 
+
+    //     this.draw = function() {
+    //         circle.beginPath();
+    //         circle.arc(this.x, this.y, this.radius, 0, 2*Math.PI);
+    //         circle.fillStyle = this.color;
+    //         circle.fill();    
+    //     }
+
+    //     this.update = function() {
+    //         if (this.x + this.radius > width || this.x - this.radius < 0) this.dx = -this.dx;
+    //         if (this.y + this.radius > height || this.y - this.radius < 0) this.dy = -this.dy;
     
-    const circleArray = [];
+    //         this.x += this.dx;
+    //         this.y += this.dy;
 
-    for (let i = 0; i < 400; i++){
+    //         this.draw();
+    //     }
+    // }
+    
+    // const circleArray = [];
+    
+    // for (let i = 0; i < 10; i++){
+    //     let radius = Math.random() * 5 + 5;
+    //     // let radius = 10;
+    //     let x = Math.random() *  (width - radius * 2) + radius;
+    //     let y = Math.random() * (height - radius * 2) + radius
+    //     let dx = (Math.random() - 0.5) * 5;
+    //     let dy = (Math.random() - 0.5) * 5;
+    //     debugger
+    //     circleArray.push(new Circ(x, y, dx, dy, radius));
+    // }
+
+    const sphereArray = [];
+    // console.log(spheres)
+    for (let i = 0; i < 500; i++){
         let radius = Math.random() * 5 + 5;
+        // let radius = 10;
         let x = Math.random() *  (width - radius * 2) + radius;
         let y = Math.random() * (height - radius * 2) + radius
         let dx = (Math.random() - 0.5) * 5;
         let dy = (Math.random() - 0.5) * 5;
-        circleArray.push(new Circ(x, y, dx, dy, radius));
+        
+        sphereArray.push(new Sphere(x, y, dx, dy, radius, canvasMain));
     }
+    
+    // function animate () {
+    //     requestAnimationFrame(animate);
+    //     circle.clearRect(0, 0, innerWidth, innerHeight);
+    //     circleArray.forEach(el => el.update());
+
+        // const COLORS = ["firebrick", "darkcyan", "green", "lime", "olive", "aquamarine", "bisque", "coral", "deeppink", "gold", "lightcoral", "lightgreen", "lightgray", "lightslategray", "springgreen", "tomato", "slateblue"]
+        // const COLORS = ["firebrick", "darkcyan", "green"]
+
+        // circleArray.forEach(el => {
+            // if (el.x + el.radius > width) {el.color = "red"}
+            // else if (el.x - el.radius < 0) {el.color = "green"}
+            // else if (el.y + el.radius > height) {el.color = "blue"}
+            // else if (el.y - el.radius < 0) {el.color = "grey"}
+
+    //         if (el.x + el.radius > width
+    //             || el.x - el.radius < 0
+    //             || el.y + el.radius > height
+    //             || el.y - el.radius < 0) 
+    //             {el.color = COLORS[Math.floor(Math.random()*COLORS.length)];}
+    //         }
+    //     );
+    // }
 
     function animate () {
         requestAnimationFrame(animate);
-        circle.clearRect(0, 0, innerWidth, innerHeight);
-        circleArray.forEach(el => el.update());
+        sphere.clearRect(0, 0, innerWidth, innerHeight);
+        sphereArray.forEach(el => el.update());
 
-        const COLORS = ["firebick", "darkcyan"]
+        // const COLORS = ["firebrick", "darkcyan", "green", "lime", "olive", "aquamarine", "bisque", "coral", "deeppink", "gold", "lightcoral", "lightgreen", "lightgray", "lightslategray", "springgreen", "tomato", "slateblue"]
+        const COLORS = ["firebrick", "darkcyan", "green"]
 
-        circleArray.forEach(el => {
-            if (el.x + el.radius > width) {el.color = "red"}
-            else if (el.x - el.radius < 0) {el.color = "green"}
-            else if (el.y + el.radius > height) {el.color = "blue"}
-            else if (el.y - el.radius < 0) {el.color = "grey"}
-            // if (el.x + el.radius > width
-            //     || el.x - el.radius < 0
-            //     || el.y + el.radius > height
-            //     || el.y - el.radius < 0) 
-            //     {el.dx *= 1.5
-            //     el.dy *= 5}
+        sphereArray.forEach(el => {
+            // if (el.x + el.radius > width) {el.color = "red"}
+            // else if (el.x - el.radius < 0) {el.color = "green"}
+            // else if (el.y + el.radius > height) {el.color = "blue"}
+            // else if (el.y - el.radius < 0) {el.color = "grey"}
+
+            if (el.x + el.radius > width
+                || el.x - el.radius < 0
+                || el.y + el.radius > height
+                || el.y - el.radius < 0) 
+                {el.color = COLORS[Math.floor(Math.random()*COLORS.length)];}
             }
         );
     }
--- src/spheres.js
+++ src/spheres.js
@@ -1,12 +1,29 @@
-function Spheres() {
+export default function Sphere(x, y, dx, dy, radius, canvas) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+    this.color = "lightgray";
+    this.canvas =  canvas;
 
-    // circle.beginPath();
-    // circle.arc(x, y, 5, 0, 2*Math.PI);
-    // circle.fillStyle = "green";
-    // circle.fill();
-    // circle.stroke;
+    this.draw = function() {
+        const sphere = this.canvas.getContext("2d");
+        sphere.beginPath();
+        sphere.arc(this.x, this.y, this.radius, 0, 2*Math.PI);
+        sphere.fillStyle = this.color;
+        sphere.fill();    
+    }
 
-    function createRandomNum() {
-        return Math.ceil(Math.random() * 299) * (Math.round(Math.random()) ? 1 : -1) / 100
-    };
+    this.update = function() {
+        if (this.x + this.radius > this.canvas.width || this.x - this.radius < 0) this.dx = -this.dx;
+        if (this.y + this.radius > this.canvas.height || this.y - this.radius < 0) this.dy = -this.dy;
+
+        this.x += this.dx;
+        this.y += this.dy;
+
+        this.draw();
+    }
 }
+
+// module.exports = Sphere;
