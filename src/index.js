6209dc573a121e43143b0407233190960bf794fb
Nick <nbarrameda89@gmail.com>
2022-02-28 06:28:12 -0500

    button styling changed for better UX/UI

--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let infected = 0
     let recovered = 0
     let simVersion = 0;
+    let simStart = null;
 
     function animate() {
         let requestID = requestAnimationFrame(animateCovid);
@@ -58,6 +59,7 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("simReset").onclick = function() {
             cancelAnimationFrame(requestID);
             document.getElementById("active").id = "inactive";
+            simStart = null;
         };    
         
         statsC();
@@ -82,6 +84,7 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("simReset").onclick = function() {
             cancelAnimationFrame(requestID);
             document.getElementById("active").id = "inactive";
+            simStart = null;
         };    
         
         statsP();
@@ -105,20 +108,38 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("simReset").onclick = function() {
             cancelAnimationFrame(requestID);
             document.getElementById("active").id = "inactive";
+            simStart = null;
         };    
         
-    }    
+    }
+
+    function scenarioButtonStyler(scenario){
+        let scenarios = ['covid', 'quarantine', 'political', 'ideas']
+
+        scenarios.forEach(scen => {
+            if (scen === scenario) {
+                document.getElementById(scenario).className = `${scenario} active`
+            } else {
+                document.getElementById(scen).className = `${scen}`
+            }
+        });
+    }
 
 
 
     document.getElementById("simStart").onclick = function() {
-        simVersion();
-        document.getElementById("inactive").id = "active";
+        if (!simStart){
+            simVersion();
+            document.getElementById("inactive").id = "active";
+            document.getElementById("simStart").className = "";
+            simStart = true;
+        }
     };
 
     document.getElementById("covid").onclick = function() {
         document.getElementById("description").innerHTML = "The speed that COVID-19 has spread is beyond what most would imagine. Observe how patient zero affects the rest of the population."
 
+        scenarioButtonStyler("covid");
         sphereArray = sim.spheres();
         sim.covidSphereUpdate(sphereArray);
         simVersion = animateCovid;
@@ -127,6 +148,7 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("quarantine").onclick = function() {
         document.getElementById("description").innerHTML = "Lockdowns imposed by governments help reduce the spread of COVID-19, but is not possible for extended periods of time."
 
+        scenarioButtonStyler("quarantine");
         sphereArray = null;
         sphereArray = sim.spheres();
         sim.quarantineSphereUpdate(sphereArray);
@@ -136,6 +158,7 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("political").onclick = function() {
         document.getElementById("description").innerHTML = "Political parties have enormous influence on their governed population as parties build and propogate ideas central to their political agenda."
 
+        scenarioButtonStyler("political");
         sphereArray = sim.spheres();
         sim.politicsSphereUpdate(sphereArray);
         simVersion = animatePolitics;
@@ -144,6 +167,7 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("ideas").onclick = function() {
         document.getElementById("description").innerHTML = "There is a diversity of ideas around us. As we share our perspectives with one another, we leave an impression that can spread, morph, and reach beyond what we've intitally thought possible."
 
+        scenarioButtonStyler("ideas");
         sphereArray = sim.spheres(200);
         sim.ideaSphereUpdate(sphereArray);
         simVersion = animateIdeas;
@@ -152,5 +176,7 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("infected").innerHTML = "";
         document.getElementById("recovered").innerHTML = "";
     };
+
+    document.getElementById("covid").click();
 
 });
